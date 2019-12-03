c64f8ee3c745dadefb29a9106985b2c4e1f13b38
Pablo Melendez <pablofer.melendez@gmail.com>
2019-12-03 14:38:49 -0500

    fix build error

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Wrapper } from '../Layout/index.styled';
+import { Wrapper } from '../../containers/Layout/index.styled';
 
 
 class Login extends Component {
